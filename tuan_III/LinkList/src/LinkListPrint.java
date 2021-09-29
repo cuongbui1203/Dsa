328585e56faa2cdc42ac318df05f664d14178170
cuongbui1203 <cuongbuimanh1203@gmail.com>
2021-09-29 10:14:07 +0700

    merge and get value

--- tuan_III/LinkList/src/LinkListPrint.java
+++ tuan_III/LinkList/src/LinkListPrint.java
@@ -1,47 +1,49 @@
+import java.util.Scanner;
+
 public class LinkListPrint {
-    static class Node{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkList ls = new LinkList();
+        for(int n = sc.nextInt();n!=0;n--){
+            ls.add(sc.nextInt());
+        }
+        ls.print(ls.head);
+        sc.close();
+    }
+
+    static class Node {
         int data;
         Node next;
-        Node(int data){
+
+        Node(int data) {
             this.data = data;
             this.next = null;
         }
     }
 
-    static class LinkList{
+    static class LinkList {
         Node head;
         Node tail;
 
-        LinkList(){
+        LinkList() {
             head = null;
             tail = null;
         }
 
-        public void add(int data){
+        public void add(int data) {
             Node tem = new Node(data);
-            if(this.head == null){
+            if (this.head == null) {
                 this.head = tem;
-            }else{
+            } else {
                 this.tail.next = tem;
             }
             this.tail = tem;
         }
 
-        public void print(Node head){
-            for (Node i = head;i!=null;i = i.next)
-                System.out.print(i.data + " ");
+        public void print(Node head) {
+            for (Node i = head; i != null; i = i.next)
+                System.out.println(i.data);
         }
-    }
-
-
-
-    public static void main(String[] args) {
-        LinkList ls = new LinkList();
-        ls.add(10);
-        ls.add(10);
-        ls.add(10);
-        ls.add(10);
-        ls.print(ls.head);
     }
 
 }
